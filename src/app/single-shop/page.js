4cd6a7e0956b419b572942d8f92f451efc1b3599
2smarthrm <2smarthrm@gmail.com>
2025-09-26 18:21:14 +0100

    Add files via upload

---
+++ src/app/single-shop/page.js
@@ -0,0 +1,18 @@
+import FooterFour from "~/components/Section/Common/FooterFour";
+import HeaderFour from "~/components/Section/Common/Header/HeaderFour";
+import PageHeader from "~/components/Section/Common/PageHeader";
+import SingleShopSection from "~/components/Section/SingleShop/SingleShopSection";
+
+const SinglePage = () => {
+  return (
+    <>
+      <HeaderFour className="tekup-header-top bg-light1 " /> 
+      <br/> <br/> <br/>
+      <SingleShopSection />
+      <FooterFour className="tekup-footer-section dark-bg" />
+    </>
+  );
+}; 
+ 
+export default SinglePage;
+ 
